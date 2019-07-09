83353e5ee908abd05d4dd968013a3cb50db4a453
Artem Arno <support@treatstock.com>
2019-07-09 15:13:20 +0300

    rename files

---
+++ examples/04_GetPrintablePackPrices.php
@@ -0,0 +1,28 @@
+<?php
+
+include '../Loader.php';
+
+$privateKey = '15ce76a20be443acfeae731cbc27dc';
+$apiService = new \treatstock\api\v2\TreatstockApiService($privateKey, 'http://ts.h3.tsdev.work');
+$apiService->setDebugMode(true);
+
+// Try create printable pack
+$createRequest = new \treatstock\api\v2\models\requests\CreatePrintablePackRequest();
+$createRequest->filePaths[] = './test.stl';
+$createRequest->locationCountryIso = 'US'; // Optional params: to get printable pack price info
+
+echo "\nSend create printable pack request...";
+$createResponse = $apiService->createPrintablePack($createRequest);
+echo "\nCreated, printable pack id: ".$createResponse->id."\n";
+
+// Sleep 5 sec, waiting for model calculations
+sleep(5);
+
+// Try get printable pack prices
+echo "\nGet printable pack prices...";
+$pricesRequest = new \treatstock\api\v2\models\requests\GetPrintablePackPricesRequest();
+$pricesRequest->printablePackId = $createResponse->id;
+$pricesResponse = $apiService->getPrintablePackPrices($pricesRequest);
+echo "\nGet printable pack prices response:\n";
+echo \treatstock\api\v2\helpers\FormattedJson::encode($pricesResponse);
+
